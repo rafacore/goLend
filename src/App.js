70ca8742bdf150834b05f95057baadedd0954fea
rafael nascimento <rafanascimento.au@gmail.com>
2019-12-24 12:45:28 +1100

    add year on route

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import Nav from './components/NavBar/Nav';
 import Vehicles from './components/pages/vehicles/Vehicles';
 import Years from './components/pages/years/Years';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {YearProvider} from './components/yearContext';
 
 import './components/styles/global.js';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
+    <YearProvider>
     <div>
       <Router>
       <Nav />
       <Route path="/years" exact component={Years} />
-      <Route path="/Vehicles" component={Vehicles} />
+      <Route path="/vehicles/:year" component={Vehicles} />
       {/* <Route path="/years/:id" component={Vehicles} /> */}
  
       </Router>
   
     </div>
+    </YearProvider>
   );
 }
 

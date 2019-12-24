ae0e28274a2994e5438f8afb6fe0a4cc913ab92f
rafael nascimento <rafanascimento.au@gmail.com>
2019-12-24 12:48:03 +1100

    change vehicles

--- src/components/pages/vehicles/Vehicles.js
+++ src/components/pages/vehicles/Vehicles.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import Header from '../../Header/Header';
-import CardHorizontal from '../../CardHorizontal/CardHorizontal';
+import React, { useContext } from "react";
+import Header from "../../Header/Header";
+import CardHorizontal from "../../CardHorizontal/CardHorizontal";
+import { useParams } from "react-router-dom";
 
-import PostData from '../../../data/posts.json';
-import YearData from '../../../data/years.json';
-import { 
-  Container,
-  Row,
-  Col,
-  } from 'reactstrap';
-import '../../styles/global';
-import imgMi from '../../../assets/ser.jpg';
-import './style.css'
+import PostData from "../../../data/posts.json";
 
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/global";
+import imgMi from "../../../assets/ser.jpg";
+import "./style.css";
 
 function Vehicles() {
-  
+  let { year } = useParams();
+
   return (
-      <Container>
-       <Header text="Let's find your car "/>
-       <Row>
-         <Col>
-         {console.log(YearData.year)}
-        {PostData.map((postDatail, index) => {
-          
-            return(
-              <CardHorizontal 
-              image={imgMi}
-              model={postDatail.model}
-              series={postDatail.series}
-              variant={postDatail.variant}
-              newPrice={postDatail.new_price}
-              priceFrom={postDatail.from} 
+    <Container>
+      <Header text="Let's find your car " />
+      <Row>
+        <Col>
+          {PostData.map((postDatail, index) => {
+            return (
+              <CardHorizontal
+                image={imgMi}
+                model={postDatail.model}
+                year={year}
+                series={postDatail.series}
+                variant={postDatail.variant}
+                newPrice={postDatail.new_price}
+                priceFrom={postDatail.from}
               />
-          
-            )
-        })}
+            );
+          })}
         </Col>
-        </Row>
+      </Row>
     </Container>
   );
 }

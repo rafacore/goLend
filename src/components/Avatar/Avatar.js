ee774c8df6672a8c2b8d7cda5121386b6252e5de
rafael nascimento <rafanascimento.au@gmail.com>
2019-12-26 16:26:47 +1100

    remove styled component

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-import userPic from '../../assets/ser.jpg';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./style.css";
 
-
-
-
-const AvatarStyled = styled.div`
-  border-radius: 50%;
-  background-color: #f7f7f7;
-  background-image: url(${props => props.src || userPic});
-  background-blend-mode: multiply;
-  background-repeat: no-repeat;
-  background-size: cover;
-  border: 10px solid #fff;
-  width: 130px;
-  height: 130px;
-  margin: 0 auto;
-  margin-bottom:25px;
-`
-const Avatar = ({image}) => {
-  return ( <AvatarStyled  src={image}/> );
-}
+const Avatar = ({ image }) => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img className="avatarImg" src={image} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Avatar;
